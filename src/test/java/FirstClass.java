0460915c1b38688c040033c68407f51d03a9a9f6
CybertekSchool <github@cybertekschool.com>
2019-11-21 16:31:50 -0500

    second commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,5 +2,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hi world");
+        System.out.println("I am working");
+        System.out.println("I am working");
     }
 }
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,6 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String myname = "Humayra";
+        System.out.println(myname);
+    }
+}
