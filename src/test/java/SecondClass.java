1e9d9bccad7af74894d3ae7edc1f912e98430842
CybertekSchool <github@cybertekschool.com>
2019-12-05 15:47:18 -0500

    changes a line

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,6 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String myname = "Humayra";
+        String myname = "John B Goodenough";
         System.out.println(myname);
         System.out.println("this  is another line");
         System.out.println("this  is another line");
